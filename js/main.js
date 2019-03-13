3a979fccf5ff0d04589b075b898d4fbc48ecd276
AntoninFB <AntoninFB>
2019-03-13 15:35:07 +0100

    correction

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ let tasks = new CompoTaskList({
     element: document.getElementById('tasks'),
     taskList: list
 });
+let list2 = new TaskList();
 let tasks2 = new CompoTaskList({
     element: document.getElementById('tasks2'),
     taskList: list
